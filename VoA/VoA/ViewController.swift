b2dd920176de56f563bb2a63dbae5a163366d7a8
linsaeng <duwjdtmd91@gmail.com>
2020-01-08 12:15:21 +0900

    Create Project

---
+++ VoA/VoA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VoA
+//
+//  Created by Jung seoung Yeo on 2020/01/08.
+//  Copyright © 2020 Linsaeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
